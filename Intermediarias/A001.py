54a2cca13af3844747f5c7a18e21582b995c0577
Ehllias <ehllias.ramalho@fatec.sp.gov.br>
2024-09-18 21:45:13 -0300

    conceitos básicos.

---
+++ Intermediarias/A001.py
@@ -0,0 +1,20 @@
+# Python básico é simplesmente tudo que dá pra fazer com as strings e seus prÂmetros de modo geral.
+
+# Class significa classe o que possa definir as informações dendtro do um objeto. como por exemplo num carro sendo a cor, marca, ano, entre outras informações.
+
+# no sentido do python, a palavra chave self simplesmente faz referêcia a outros atributos, nesse sentido pode ser usado por exemplo com o _init_ para que se tenha resultados promissores.
+
+# O método _init_ é usado como um construtor que faz a definição de atributos nas instâncias ou diretamente nas classes.
+
+# Exemplo
+class Carro:
+    def __init__(self, marca, modelo):
+        self.marca = marca
+        self.modelo = modelo
+
+    def descricao(self):
+        return f"Este carro é um {self.marca} {self.modelo}."
+
+# Criando uma instância da classe Carro
+meu_carro = Carro("Toyota", "Corolla")
+print(meu_carro.descricao())  # Saída: Este carro é um Toyota Corolla.
