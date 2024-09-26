2558cf2bdf45fc7ae1abe6c48435dad37b8e4a31
Ehllias <ehllias.ramalho@fatec.sp.gov.br>
2024-09-26 11:18:45 -0300

    Sorveteria

--- Aula2609/Classe.py
+++ Aula2609/Classe.py
@@ -1,10 +1,15 @@
-class cachorro:
-    def __init__(self, raca, nome, idade, genero):
+class animal:
+    def _init_(self, raca, som, genero)
         self.raca = raca
+        self.som = som
+        self.genero = genero
+def _init_ (self)
+print(f"{self.raca} está {self.som}")
+class cachorro(animal):
+    def __init__(self, nome, idade, raca, som, genero):
+        super().__init__(raca, som, genero):
         self.nome = nome
         self.idade = idade
-        self.genero = genero
-
     def sentar(self):
         print(f"{self.nome} está sentado")
 
--- Aula2609/Restaurante.py
+++ Aula2609/Restaurante.py
@@ -21,3 +21,14 @@ class restaurante:
 
 meurestaurante = restaurante("Lasanha","Av. Paulo Scander 192","19(99999-9999)")
 meurestaurante.cozinha()
+
+class sorveteria(restaurante):
+    def  __init__(self, cardapio, endereco, telefone, sabores):
+        super().__init__(cardapio, endereco, telefone)
+        self.sabores = sabores
+
+    def imprimir_sabores(self):
+        print(f"Os sabores do dia são {self.sabores}, e lembr-se que nós estamos localizados em {self.endereco}, e nosso telefone é {self.telefone}.")
+
+sorvete = sorveteria("Menta com chocolate","Av. Paulo Scander 192","19(99999-9999)","Leite-ninho, chocolate, baunilha")
+sorvete.imprimir_sabores()
